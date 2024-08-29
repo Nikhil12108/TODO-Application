cc5970e04fcefb43a31489736d1a4e111a2da857
Nikhil Kumar <nikhilkumar84157@gmail.com>
2024-08-29 19:18:05 +0530

    added zod validation

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const port = 3000
 const app = express()
+const {createTodo, updateTodo} = require("./types")
 app.use(express.json());
 
 
 app.post('/todo' ,(req, res) => {
-
+    const createPayLoad = req.body;
+    const parsedPayLoad = createTodo.safeParse(createPayLoad);
+    if(!parsedPayLoad.success) {
+        res.status(411).json({
+            msg: "wring input"
+        })
+        return;
+    }
+    // put it in mongodb
 })
 
 app.get('/todos', (req, res) => {
@@ -13,7 +22,14 @@ app.get('/todos', (req, res) => {
 })
 
 app.put('/completed', (req, res) => {
-
+    const updatePayLoad = req.body;
+    const parsedPayLoad = updateTodo.safeParse(updatePayLoad);
+    if(!parsedPayLoad.success) {
+        res.status(411).json({
+            msg: "wrong inputs",
+        })
+        return;
+    }
 })
 
 app.listen(port, () => {
@@ -24,3 +40,4 @@ app.listen(port, () => {
 // step 1 basic express boilerpate code
 // step 2 json middleware
 // step 3 zod validation
+// step 4 create a mongoDB schema
